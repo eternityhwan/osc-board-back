4445e0383b02867446bc7d84d725a2ac1057dbf8
eternityhwan <mirrorkyh@gmail.com>
2022-10-24 11:34:23 +0900

    revision dockerfile

--- src/test/java/com/osckorea/board/jasyptConfigTest.java
+++ src/test/java/com/osckorea/board/jasyptConfigTest.java
@@ -1,35 +1,35 @@
-package com.osckorea.board;
+//package com.osckorea.board;
+//
+//import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest
+//class jasyptConfigTest {
+//
+//    @Test
+//    void stringEncryptor() {
+//    }
+//
+//    @Test
+//    void jasypt() {
 
-import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
-class jasyptConfigTest {
-
-    @Test
-    void stringEncryptor() {
-    }
-
-    @Test
-    void jasypt() {
-
-        System.out.println(jasyptEncoding(url));
-        System.out.println("-----------------------------------");
-        System.out.println(jasyptEncoding(username));
-        System.out.println("-----------------------------------");
-        System.out.println(jasyptEncoding(password));
-        System.out.println("-----------------------------------");
-    }
-
-    public String jasyptEncoding(String value) {
-
-        String key = "my_jasypt_key";
-        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
-        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
-        pbeEnc.setPassword(key);
-        return pbeEnc.encrypt(value);
-    }
-}
+//        System.out.println(jasyptEncoding(url));
+//        System.out.println("-----------------------------------");
+//        System.out.println(jasyptEncoding(username));
+//        System.out.println("-----------------------------------");
+//        System.out.println(jasyptEncoding(password));
+//        System.out.println("-----------------------------------");
+//    }
+//
+//    public String jasyptEncoding(String value) {
+//
+//        String key = "my_jasypt_key";
+//        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+//        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+//        pbeEnc.setPassword(key);
+//        return pbeEnc.encrypt(value);
+//    }
+//}
